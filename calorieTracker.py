c1413074fe8a2bc39bbb65fd76dd52bc0387a813
Allan Khariton <allankhariton@gmail.com>
2022-05-18 22:10:14 -0400

    added first graph

--- calorieTracker.py
+++ calorieTracker.py
@@ -8,12 +8,30 @@ import plotly.graph_objs as go
 from dash.dependencies import Input, Output, State
 import requests
 import plotly.express as px
+import numpy as np
 
 app = dash.Dash()
-server=app.server
+# server=app.server
 
+df = pd.read_csv('./calorie_df.csv')
 
+df['Daily_Green'] = df['Breakfast - Green']+df['Lunch - Green']+df['Dinner - Green']+df['Snacks - Green']
+df['Daily_Yellow'] = df['Breakfast - Yellow']+df['Lunch - Yellow']+df['Dinner - Yellow']+df['Snacks - Yellow']
+df['Daily_Red'] = df['Breakfast - Red']+df['Lunch - Red']+df['Dinner - Red']+df['Snacks - Red']
 
+colors = ['green','gold','red']
+fig = px.bar(df, x="Date", y=["Daily_Green", "Daily_Yellow", "Daily_Red"], title="Daily Calorie Density Intake Breakdown")
+
+# fig = go.Figure(data=[go.Bar(
+#     x=df['Date'],
+#     y=,
+#     marker_color=colors # marker color can be a single color value or an iterable
+# )])
+# fig.update_layout(title_text='Least Used Feature')
+
+app.layout = html.Div([dcc.Graph(figure=fig)
+
+])
 
 
 if __name__=="__main__":
